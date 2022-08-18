c5166e592a577f76e2ed21a6546178e85a15ad09
Gabriel Matos <gabrielmatos010203@gmail.com>
2022-08-18 18:43:54 -0300

    corrigindo o erro de renderização

--- Projeto-AirCnC/mobile/src/components/SpotList.js
+++ Projeto-AirCnC/mobile/src/components/SpotList.js
@@ -38,7 +38,7 @@ function SpotList({tech ,navigation}){
                 showsHorizontalScrollIndicator={false}
                 renderItem={({ item }) => (
                     <View style={styles.listItem}>
-                      <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url }} />
+                      <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url.replace('localhost','192.168.15.49') }} />
                       <Text style={styles.company}>{item.company}</Text>
                       <Text style={styles.price}>{item.price ? `R$${item.price}/dia` : 'GRATUITO'}</Text>
                       <TouchableOpacity onPress={() => handleNavigate(item._id)} style={styles.button}>
